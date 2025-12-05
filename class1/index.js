694b07e1d2cf103077c420273fcd0fcc2eb3265b
ofctarun <narayanashettitarun01@gmail.com>
2025-12-05 11:20:42 +0530

    ignoring node modules

--- class1/index.js
+++ class1/index.js
@@ -1,18 +1,26 @@
-import http from 'http';
+// import http from 'http';
 
-const server = http.createServer((req, res) => {
-    console.log(req.method)
-    if (req.url === "/users") {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ name: "thub" }))
-    }
-    else {
-        res.writeHead(200, { "content-type": "text/plain" });
-        res.end("Hello this is from backend");
-    }
+// const server = http.createServer((req, res) => {
+//     console.log(req.method)
+//     if (req.url === "/users") {
+//         res.writeHead(200, { "Content-Type": "application/json" });
+//         res.end(JSON.stringify({ name: "thub" }))
+//     }
+//     else {
+//         res.writeHead(200, { "content-type": "text/plain" });
+//         res.end("Hello this is from backend");
+//     }
 
-});
+// });
 
-server.listen(7007, () => {
-    console.log(`server running at port ${7007}`)
-});
+// server.listen(7007, () => {
+//     console.log(`server running at port ${7007}`)
+// });
+
+import express from "express";
+
+const app = express();
+
+app.listen(7007, () => {
+    console.log("server running at 7007")
+})
